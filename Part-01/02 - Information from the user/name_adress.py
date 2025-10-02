c39094c82a572ae8608436b17436d24b508af7fb
Syed Salahuddin <humbleakh21@gmail.com>
2025-10-02 15:14:54 +0530

    Added 2 part exercises of part1

---
+++ Part-01/02 - Information from the user/name_adress.py
@@ -0,0 +1,8 @@
+# Write your solution here
+given_name = input("Given name: ")
+family_name = input("Family name:")
+address = input("Street address: ")
+city_code = input("City and postal code: ")
+print(f"{given_name} {family_name}")
+print(address)
+print(city_code)
